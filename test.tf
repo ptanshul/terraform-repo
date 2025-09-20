d9c938c11dac317dcafd41ced913d69c0ef74f74
anshulmb <anshultiwarimb@gmail.com>
2025-09-20 17:10:18 +0530

    ctest commit2

--- test.tf
+++ test.tf
@@ -1,2 +1,16 @@
 # terraform tf files
 # Use this file to define your infrastructure as code.
+# For example, you can define resources, variables, and outputs here.
+# Example resource definition:
+# resource "aws_instance" "example" {
+#   ami           = "ami-0c55b159cbfafe1f0"
+#   instance_type = "t2.micro"
+# }
+# Example variable definition:
+# variable "instance_count" {
+#   type    = number
+#   default = 1
+# }
+# Example output definition:
+# output "instance_id" {
+#   value = aws_instance.example.id
