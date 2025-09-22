e83cc93cef1c9a5e780ddcdbe4f6bb17b0221314
anshulmb <anshultiwarimb@gmail.com>
2025-09-22 11:35:29 +0530

    boto1

---
+++ python2.py
@@ -0,0 +1,11 @@
+import boto3
+
+# Create an S3 client
+s3 = boto3.client("s3")
+
+# List all buckets
+response = s3.list_buckets()
+
+print("S3 Buckets:")
+for bucket in response['Buckets']:
+    print(f" - {bucket['Name']}")
